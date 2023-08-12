f4cd66d300c8b98a1cb5ce0bc41bf65cf461df0a
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-08-12 14:43:53 -0300

    FEAT: New script

---
+++ remove_all_containers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Get the all containers ID
+containers_ids=($(docker ps -a | grep -v "CONTAINER" | awk '{print $1}'))
+
+
+# Iterate over the list and remove all containers
+for container_id in ${containers_ids[@]}; do
+	docker container rm $container_id
+done
